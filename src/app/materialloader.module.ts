5896e7cd832ac37247e18b5b1a9b0a72971fcf5b
DerZade <DerZade@gmail.com>
2018-07-29 23:38:25 +0200

    Added more material components

--- src/app/materialloader.module.ts
+++ src/app/materialloader.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatSlideToggleModule, MatChipsModule, MatExpansionModule, MatRadioModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatSlideToggleModule, MatChipsModule, MatExpansionModule, MatRadioModule],
+  exports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule, MatSlideToggleModule, MatChipsModule, MatExpansionModule, MatRadioModule]
 })
 export class MaterialLoaderModule { }
